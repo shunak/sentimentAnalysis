ce25f7f47e05ad96a218780b3fd29a866bd25c3d
DESKTOP-BRIGC4V\USER <shuna966@gmail.com>
2018-08-09 00:00:11 +0900

    180808

--- sentiment-lstm.py
+++ sentiment-lstm.py
@@ -31,9 +31,41 @@ vocab=sorted(counts, key=counts.get, reverse=True) #get words by desc
 vocab_to_int = {word:ii for ii,word in enumerate(vocab,1)} # get key and value by enumerate
 # print(vocab_to_int) 
 
-reviews_int=[]
+reviews_int=[] # convert reviews literal to int
 for each in reviews:
     reviews_int.append([vocab_to_int[word] for word in each.split()])
 
-print(reviews_int)
+# print(reviews_int)
+# convert label to vector 
+labels = labels.split('\n')
+labels = np.array([1 if each == 'positive' else 0 for each in labels])
+
+# print(labels[:50])
+review_lens =Counter([len(x) for x in reviews_int])
+# print(review_lens[0]) 
+# print(max(review_lens))
+non_zero_idx = [ii for ii, review in enumerate(reviews_int) if len(review)!=0]
+
+revies_int = [reviews_int[ii] for ii in non_zero_idx]
+labels = np.array([labels[ii] for ii in non_zero_idx])
+# print(len(non_zero_idx))
+# print(reviews_int[-1])
+seq_len = 200
+features =np.zeros((len(reviews_int), seq_len),dtype=int)
+
+for i, row in enumerate(reviews_int):
+    features[i,-len(row):]=np.array(row)[:seq_len]
+
+
+
+
+
+
+
+print(features[:10,:100])
+
+
+
+
+
 
